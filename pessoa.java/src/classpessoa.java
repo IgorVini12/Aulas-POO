7bbc710623e7f4ea538885aff23032e5d3cb6ceb
IgorVini12 <57765347+IgorVini12@users.noreply.github.com>
2026-03-09 08:53:40 -0300

    Aula02

---
+++ pessoa.java/src/classpessoa.java
@@ -0,0 +1,14 @@
+public class classpessoa {
+    // Atributos
+String nome;
+int idade;
+double renda;
+
+classpessoa(){}
+
+classpessoa(String n, int i, double r){
+   nome = n;
+   idade = i;
+   renda = r;
+   }
+}
