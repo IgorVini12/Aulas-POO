6968beaab5186eaacb4cbf24e0abfcf83a585b15
IgorVini12 <57765347+IgorVini12@users.noreply.github.com>
2026-03-16 09:00:41 -0300

    Add files via upload

---
+++ TesteGrafico/pessoa.java
@@ -0,0 +1,17 @@
+package TesteGrafico;
+
+public class pessoa {
+    // Atributos
+    String nome;
+    int idade;
+    double renda;
+
+    pessoa() {
+    }
+
+    pessoa(String n, int i, double r) {
+        nome = n;
+        idade = i;
+        renda = r;
+    }
+}
