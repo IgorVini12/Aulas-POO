1b12df8f61e5ae5c1326116dbc46f5a4eb795aab
igor <seuemail@exemplo.com>
2026-03-16 10:45:56 -0300

    Atualiza as classes Fatura e TesteFatura para corrigir pontuação e adicionar métodos de soma e atualização de saldo

--- fatura/src/Fatura.java
+++ fatura/src/Fatura.java
@@ -1,16 +1,41 @@
 public class Fatura {
+    public static final Fatura NovaFatura = null;
     float valor;
-    int pontuação;
+    int pontuacao;
     String tipoConta;
 
-    Fatura(){
+    Fatura() {
         valor = 0;
-        pontuação = 0;
-        tipoConta  = "Normal";
+        pontuacao = 0;
+        tipoConta = "Normal";
     }
-    Fatura(float valor, int pontuação, String tipoConta){
+
+    Fatura(float valor, int pontuacao, String tipoConta) {
         this.valor = valor;
-        this.pontuação = pontuação;
-        this.tipoConta  = tipoConta;
-    } 
+        this.pontuacao = pontuacao;
+        this.tipoConta = tipoConta;
+    }
+
+    void adicionaCompra(float valor, float f) {
+        this.valor += valor; // this.valor = this.valor + valor
+    }
+
+    Fatura somaCom(float valor) { // soma duas faturas
+        Fatura NovaFatura = new Fatura(); // vou criar uma "NovaFatura" com base na new Fatura
+        NovaFatura.valor = this.valor + valor;
+        return NovaFatura;
+    }
+
+    Fatura somaCom(int pontuacao) {
+        Fatura NovaFatura = new Fatura();
+        NovaFatura.pontuacao = this.pontuacao + pontuacao;
+        return NovaFatura;
+    }
+     Fatura somaCom(int pontuacao, float valor) {
+        Fatura NovaFatura = new Fatura();
+        NovaFatura.pontuacao = this.pontuacao + pontuacao;
+        NovaFatura.valor = this.valor + valor;
+        return NovaFatura;
+    }
+
 }
--- fatura/src/TesteFatura.java
+++ fatura/src/TesteFatura.java
@@ -1,11 +1,21 @@
 public class TesteFatura {
- public static void main(String[] args) {
-    Fatura f1 = new Fatura();
-    System.out.println("Valor da fatura 1: R$ " + f1.valor);
-    f1.valor = 100;
-     System.out.println("Valor da fatura 1: R$ " + f1.valor);
-    
-     Fatura f2 = new Fatura(2000, 60, null);
-     System.out.println("Valor da fatura 2: R$ " + f2.valor); 
- }   
+    public static void main(String[] args) {
+        Fatura f1 = new Fatura();
+        System.out.println("Valor da fatura 1: R$ " + f1.valor);
+        f1.valor = 100;
+        System.out.println("Valor da fatura 1: R$ " + f1.valor);
+
+        Fatura f2 = new Fatura(2000, 60, null);
+        System.out.println("Valor da fatura 2: R$ " + f2.valor);
+        f2.adicionaCompra(349, 99f);
+        System.out.println("Saldo atualizado da fatura 2: " + f2.valor);
+
+        Fatura f3 = f2.somaCom(400f);
+        System.out.println("Saldo da Fatura 3: " + f3.valor);
+        System.out.println("Saldo da Fatura 2: " + f2.valor);
+
+        Fatura f4 = f2.somaCom(50);
+        Fatura f5 = f2.somaCom(50, 400f);
+
+    }
 }
