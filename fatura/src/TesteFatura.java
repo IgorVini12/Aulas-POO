e5c385cb93132874fa72813e6fa7715fd2492ed9
igor <seuemail@exemplo.com>
2026-03-16 10:25:11 -0300

    Atualiza a classe TesteFatura para exibir corretamente os valores das faturas

--- fatura/src/TesteFatura.java
+++ fatura/src/TesteFatura.java
@@ -3,6 +3,9 @@ public class TesteFatura {
     Fatura f1 = new Fatura();
     System.out.println("Valor da fatura 1: R$ " + f1.valor);
     f1.valor = 100;
-     System.out.println("Valor da fatura 1: R$ " + f1.valor); 
+     System.out.println("Valor da fatura 1: R$ " + f1.valor);
+    
+     Fatura f2 = new Fatura(2000, 60, null);
+     System.out.println("Valor da fatura 2: R$ " + f2.valor); 
  }   
 }
