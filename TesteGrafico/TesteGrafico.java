8a021c03bc6db677dfb01a51b06f144a88773d53
igor <seuemail@exemplo.com>
2026-03-16 09:29:05 -0300

    Adiciona classes TesteGrafico e pessoa para entrada e exibição de dados do usuário

---
+++ TesteGrafico/TesteGrafico.java
@@ -0,0 +1,17 @@
+package TesteGrafico;
+
+import javax.swing.JOptionPane;
+public class TesteGrafico {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog(null,"Digite seu nome: ");
+        String idadeStr = JOptionPane.showInputDialog(null,"Digite sua idade: ");
+        String rendaStr = JOptionPane.showInputDialog(null,"Digite sua renda: ");
+
+        int idade = Integer.parseInt(idadeStr);
+        double renda = Double.parseDouble(rendaStr);
+
+        pessoa p1 = new pessoa(nome, idade, renda);
+
+        JOptionPane.showMessageDialog(null, "Nome: " + p1.nome + "\nIdade: " + p1.idade + "\nRenda: " + p1.renda);
+    }
+}
