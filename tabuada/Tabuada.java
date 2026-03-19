366912b2a51646e52038f8d05fdaa8ff92a3c09b
igor vinicius <seuemail@exemplo.com>
2026-03-18 23:24:41 -0300

    Corrige a declaração da variável QuantidadeDeAlunos e ajusta a estrutura do construtor da classe classeCurso

---
+++ tabuada/Tabuada.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        new Tabuada();
+    }
+
+    public Tabuada() {
+        int numero = 0;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um número para ver a tabuada: ");
+        numero = teclado.nextInt();
+        imprimaTabuada(numero);
+    }
+
+    public void imprimaTabuada(int numero) {
+        for (int x = 1; x <= 10; x++) {
+            System.out.println(numero + " x " + x + " = " + (numero * x));
+        }
+    }
+}
